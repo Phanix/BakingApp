7aaf846d48fa20b858890dc494914b54fe6ef2db
fabiodasilva <fh2game86@gmail.com>
2019-04-26 00:33:58 -0300

    update 1

--- app/src/main/java/hantaro/com/bakingapp/RecipeDetails.java
+++ app/src/main/java/hantaro/com/bakingapp/RecipeDetails.java
@@ -27,38 +27,15 @@ public class RecipeDetails extends AppCompatActivity {
             setTitle(recipe.getName());
             Log.i("Name", recipe.getName());
         }
-        final List<Step> stepList = recipe.getSteps();
-        final List<String> stepsName = new ArrayList<>();
-        final List<String> ingredients = new ArrayList<>();
-        for(Step step : recipe.getSteps()){
-            stepsName.add( step.getId() + " " + step.getShortDescription());
-        }
-        stepsName.remove(0);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stepsName);
-        ListView listView = findViewById(R.id.simple_list_view);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent1 = new Intent(RecipeDetails.this, RecipeStep.class);
-                intent1.putExtra("step", stepList.get(i));
-                startActivity(intent1);
-            }
-        });
-        listView.setAdapter(adapter);
 
-        final Recipe recipe1 = recipe;
+        MasterListFragment masterListFragment = new MasterListFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("recipe", recipe);
+        masterListFragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction().add(R.id.master_list_id, masterListFragment).commit();
 
 
-        final TextView ingredientsTV = findViewById(R.id.ingredients_tv);
-        ingredientsTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //call new intent
-                Intent intent1 = new Intent(RecipeDetails.this, IngredientsDetails.class);
-                intent1.putExtra("recipe", recipe1);
-                startActivity(intent1);
-            }
-        });
+
 
     }
 
